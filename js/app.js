699afc5b5f4971d205d380a7312983a33ba8517e
김민성 <46954944+minsung03@users.noreply.github.com>
2020-11-19 15:03:00 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
+let wrap = document.querySelector("#wrap");
+
 fetch("./json/NBA-Player-notice.json")
 .then(res => res.json())
 .then(json => {
     // console.log(JSON.stringify(json.lscd));
-    console.log(JSON.stringify(json.lscd[0].mscd.g[0].ptsls.pl[0].ln));
+    // console.log(json);
+    console.log(json);
+    JSON.stringify(json).replace(/\"/g, "");
+    console.log(json.lscd[0].mscd.g[0].ptsls.pl[0].img);
+    document.querySelector(".player_name").append(`
+        ${JSON.stringify(json).replace(/\"/g, "")}
+        <img src="./img/player_profile/${json.lscd[0].mscd.g[0].ptsls.pl[0].img[0]}" alt="player_name">
+    `);
 });
